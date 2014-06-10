bbfe2ac24915e7ca60b656f4c05c50eded9aa5ad
Roger Camps <rogercampsny@gmail.com>
2014-06-09 23:39:00 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wheel::Application.config.secret_key_base = 'c539f6eafa0af49117f5926e23003ebe1a7af20331edb0e56b50718bd2d69811536380caad97ecf4b54e75c9d09eb473999083f5e96099920482d6a3c654dfa3'
